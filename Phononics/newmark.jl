6af14aa3b01884b217ea580dc4369cf4bc1babd8
arturofburgos <burgos.arturo.ufu@gmail.com>
2022-10-25 14:37:29 -0500

    newmark

--- Phononics/newmark.jl
+++ Phononics/newmark.jl
@@ -7,13 +7,15 @@
 #========================================#
 
 # Second order ODE: Harmonic Oscillation - nm spring and nm mass
+
+
 using LinearAlgebra, LinearMaps, Plots
 #============#
 # USER INPUT #
 #============#
 
-T = 25
-dt = 0.02 
+T = 5
+dt = 0.005 
 # Here define the number of pair spring-mass: 
 nm = 3
 # Assuming all the masses are equal:
@@ -21,13 +23,19 @@ m = 3
 # Assuming all springs are equal:
 k = 8
 
+# γ and β parameters:
+γ = 1/2
+β = 1/4
+
+
+
 # Initial Quantities
 # Note that we should assign nm quatities for position and velocity
 
 # Initial Position
-x0 = [0.0 0.2 0.4] 
+x0 = [0.1 0.0 0.0] 
 # Initial Velocity
-ẋ0 = [0.1 0.5 1.1]
+ẋ0 = [0.0 0 0]
 
 
 #===========================#
@@ -39,14 +47,14 @@ N = Int(T/dt) + 1 # Number of time-steps
 t = range(0, T, N)
 
 # Define the zero matrix:
-O = zeros(nm, nm)
+#O = zeros(nm, nm)
 
 # Define M̃ matrix
-m_vec = fill(m, nm)
-M̃ = diagm(m_vec)
+#m_vec = fill(m, nm)
+#M̃ = diagm(m_vec)
 
 # Define big M matrix 
-M = [I O; O M̃]
+#M = [I O; O M̃]
 
 # Define A and At matrices: We either can create the actual A matrix
 # Or we can apply a function Atimes! that maps a matrix. 
@@ -55,7 +63,7 @@ M = [I O; O M̃]
 # Matrix #
 #========#
 
-A = zeros(nm,nm)
+#= A = zeros(nm,nm)
 
 for i in 1:nm
     for j in 1:nm
@@ -68,7 +76,7 @@ for i in 1:nm
     end 
 end
 
-At = A'
+At = A' =#
 
 #===========#
 # LinearMap #
@@ -124,25 +132,40 @@ end
 A = LinearMap(A_times!,A_times_T!, nm) =#
 
 # Define K̃ matrix
-k_vec = fill(k,nm)
-K̃ = diagm(k_vec)
+#k_vec = fill(k,nm)
+#K̃ = diagm(k_vec)
 
 # Define K̂
-K̂ = A'*K̃*A
+#K̂ = A'*K̃*A
 
 # Define big K matrix
-K = [O -I; K̂ O]
+#K = [O -I; K̂ O]
 
 # Define big C matrix
-C = (-inv(M) * K)
+#C = (-inv(M) * K)
+
+M̃ = [10 0 0; 0 20 0; 0 0 30]
+K̂ = 1e3*[45 -20 -15;-20 45 -25;-15 -25 40]
+
+# Define P matrix
+
+
+P = M̃/(β*dt^2) + K̂
+P_inv = inv(P)
 
 # Define State Space representation u:
-u = [x0 ẋ0]'
+u = zeros(nm,N)
+ud = zeros(nm,N)
+udd = zeros(nm,N)
+
+u[:,1] = x0
+ud[:,1] = ẋ0
+udd[:,1]= -inv(M̃)*(K̂*u[:,1])
 
 # Defining solution array:
-x = zeros(N,2*nm)
+#x = zeros(N,2*nm)
 # Assingning the initial conditions to solution array
-x[1,:] = [x0 ẋ0]
+#x[1,:] = [x0 ẋ0]
 
 
 #======================#
@@ -201,13 +224,28 @@ function trapezoidal(y, h, N, A, save_var)
     return save_var
 end
 
+
+#================#
+# Newmark Scheme #
+#================#
+
+for i in 1:N-1 
+    Q = M̃*((1/(β*dt^2))*u[:,i] + (1/(β*dt))*ud[:,i] + (1/(2*β) - 1)*udd[:,i])
+    if i < 5 
+        @show Q
+    end
+    u[:, i+1] = inv(P)*Q 
+    udd[:, i+1] = (1/(β*dt^2))*(u[:,i+1]-u[:,i]) - (1/(2*β)-1)*ud[:,i]-((1/(2*β))-1)*udd[:,i]
+    ud[:, i+1] = ud[:,i] + (1-γ)*dt*udd[:,i] + γ*dt*udd[:, i+1]
+end
+
 #x = forward_euler(u, dt, N, C, x)
 #x = backward_euler(u, dt, N, C, x)
-x = trapezoidal(u, dt, N, C, x)
+#x = trapezoidal(u, dt, N, C, x)
 
 
 #=================#
 # Post Processing #
 #=================#
 
-plot(t,x[:,1:nm])
+plot(t[:],u[1,:])
